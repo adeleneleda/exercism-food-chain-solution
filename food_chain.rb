9562a73eecddbe2de6426efd8ba3a8ea4650fba4
apfestin <festinadelen@gmail.com>
2017-06-20 18:38:07 +0800

    Initial commit. Setup class and main function.

---
+++ food_chain.rb
@@ -0,0 +1,13 @@
+"""
+food_chain = FoodChain.new
+food_chain.generate_song
+
+"""
+class FoodChain
+  SUBJECTS = [
+    {food: 'fly', line: ''}
+  ]
+  def generate_song
+    puts "GENERATE SONG"
+  end
+end
