9466a5a650c8e981ba57e591b5e75a48a602e30a
apfestin <festinadelen@gmail.com>
2017-06-20 19:51:06 +0800

    Add test. All tests passing

--- food_chain.rb
+++ food_chain.rb
@@ -24,51 +24,60 @@ class FoodChain
     end
   end
 
-  def generate_song
-    previous = []
-    
-    SUBJECTS.each_with_index do |subject, index|
-      swallow(subject)
-      previous << subject[:food]
-      generate_midverse(previous.reverse, index)
-      generate_fly_line(index)
-      puts ""
+  class << self
+    def song
+      lines = []
+      previous = []
+
+      SUBJECTS.each_with_index do |subject, index|
+        previous << subject[:food]
+
+        lines << swallow(subject)
+        lines << generate_midverse(previous.reverse, index)
+        lines << generate_fly_line(index)
+        lines << "" unless last?(index)
+      end
+
+      lines.flatten.compact.join("\n")
     end
-  end
 
-  def generate_midverse(previous_subjects, index)
-    previous_subjects.each_with_index do |sub, i|
-      swallow_and_catch(sub, previous_subjects[i+1]) if non_boundary?(index) && i != index
+    def generate_midverse(previous_subjects, index)
+      line = []
+      previous_subjects.each_with_index do |sub, i|
+        line << swallow_and_catch(sub, previous_subjects[i+1]) if non_boundary?(index) && i != index
+      end
+
+      line
     end
-  end
 
-  def generate_fly_line(index)
-    puts SUBJECTS[0][:line] if non_boundary?(index)
-  end
+    def generate_fly_line(index)
+      SUBJECTS[0][:line] if non_boundary?(index)
+    end
 
-  def swallow(subject)
-    puts "I know an old lady who swallowed a #{ subject[:food] }."
-    puts subject[:line]
-  end
+    def swallow(subject)
+      line = [
+                "I know an old lady who swallowed a #{ subject[:food] }.",
+                subject[:line]
+              ]
+    end
 
-  def swallow_and_catch(main, prey)
-    print "She swallowed the #{ main } to catch the #{ prey }"
-    print " that wriggled and jiggled and tickled inside her" if prey == "spider"
-    puts "."
-  end
+    def swallow_and_catch(main, prey)
+      line = ""
+      line += "She swallowed the #{ main } to catch the #{ prey }"
+      line += " that wriggled and jiggled and tickled inside her" if prey == "spider"
+      line += "."
+    end
 
-  def non_boundary?(index)
-    !_first?(index) && !_last?(index)
-  end
+    def non_boundary?(index)
+      !first?(index) && !last?(index)
+    end
 
-  def _first?(index)
-    index == 0
-  end
+    def first?(index)
+      index == 0
+    end
 
-  def _last?(index)
-    index == (SUBJECTS.length - 1)
+    def last?(index)
+      index == (SUBJECTS.length - 1)
+    end
   end
 end
-
-food_chain = FoodChain.new
-food_chain.generate_song
